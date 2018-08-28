0b236341da2193e464411ba1532fc7730276b696
hsadoqi@gmail.com <hsadoqi@gmail.com>
2018-08-28 03:37:06 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,9 @@ def roll_call_dwarves(names)
   names.each_with_index{|name, index| puts "#{index+1}. #{name}"}
 end 
 
-def summon_captain_planet()
+def summon_captain_planet(calls)
+  long_calls = calls.collect {|call| call.capitalize + "!"}
+  long_calls 
 end 
 
 def long_planeteer_calls 
