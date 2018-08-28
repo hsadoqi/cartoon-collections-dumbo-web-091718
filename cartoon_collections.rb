5a25769fcc83f6ca6cd99a2e2ce63de2efb2803b
hsadoqi@gmail.com <hsadoqi@gmail.com>
2018-08-28 03:39:04 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,9 +10,9 @@ end
 def long_planeteer_calls(calls)
   calls.each do |call|
     if call.length > 4
-      return true 
+      true 
     else 
-      return false 
+      false 
     end 
   end
 end 
